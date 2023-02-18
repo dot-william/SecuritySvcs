25e615a91aeeeb40a029844a7fccca325a7f9525
Simon Que <simon_justin_que@dlsu.edu.ph>
2023-02-18 15:08:27 +0800

    added hashing

--- CSSECDV MP/src/Model/User.java
+++ CSSECDV MP/src/Model/User.java
@@ -1,21 +1,28 @@
 package Model;
+import java.security.SecureRandom;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class User {
     private int id;
     private String username;
-    private String password;
+    private String passwordhash;
     private int role = 2;
     private int locked = 0;
-
+    
     public User(String username, String password){
         this.username = username;
-        this.password = password;
+        this.passwordhash = password;
+        
     }
     
     public User(int id, String username, String password, int role, int locked){
         this.id = id;
         this.username = username;
-        this.password = password;
+        this.passwordhash = password;
         this.role = role;
         this.locked = locked;
     }
@@ -36,12 +43,27 @@ public class User {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
+    public String getPasswordHash() {
+        return this.passwordhash;
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        // create secure RNG
+        SecureRandom random = new SecureRandom(); 
+        // create 16-byte salt
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        System.out.println(salt);
+        // hash the password and the salt
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128); 
+            SecretKey key = factory.generateSecret(spec);
+            byte[] hash = key.getEncoded();
+            this.passwordhash = new String(hash);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public int getRole() {
